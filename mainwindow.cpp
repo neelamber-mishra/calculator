5185dc9b9e83522234f748ce1bd111a9dcb0342e
neelamber mishra <24je0663@iitism.ac.in>
2025-04-17 18:29:34 +0530

    added basic functions

---
+++ mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+int a,b,s;
+
+void MainWindow::on_add_clicked()
+{
+    a = ui->a->text().toInt();
+    b = ui->b->text().toInt();
+    s = a+b;
+    ui->result->setText(QString::number(s));
+}
+
+
+void MainWindow::on_sub_clicked()
+{
+     ui->result->setText(QString::number(0));
+}
+
+
+void MainWindow::on_mul_clicked()
+{
+     ui->result->setText(QString::number(0));
+}
+
+
+void MainWindow::on_div_clicked()
+{
+     ui->result->setText(QString::number(0));
+}
+
